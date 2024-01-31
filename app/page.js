a6ea8dafd11042771638e701ef50972d630ff3c2
ozlemkayasaroglu <ozlemkayasaroglu@gmail.com>
2024-01-31 14:24:56 +0300

    first and last commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,119 @@
+"use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const [jokes, setJokes] = useState([]);
+  const [currentJokeIndex, setCurrentJokeIndex] = useState(0);
+
+  const getNextJoke = () => {
+    // Rastgele bir şaka indeksi seç
+    const randomIndex = Math.floor(Math.random() * jokes.length);
+
+    // Seçilen indeksi güncelle
+    setCurrentJokeIndex(randomIndex);
+  };
+
+  useEffect(() => {
+    async function fetchJokes() {
+      try {
+        const response = await fetch("/api/jokes");
+        if (!response.ok) {
+          throw new Error("veri alınamadı");
+        }
+
+        const data = await response.json();
+        setJokes(data.data);
+      } catch (error) {
+        console.error("fetch hatası", error);
+      }
+    }
+    fetchJokes();
+  }, []);
+
+  if (jokes.length === 0) {
+    return <div>Şakalar yükleniyor...</div>;
+  }
+
+  const currentJoke = jokes[currentJokeIndex];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <>
+      <div className="fixed inset-0 z-10 flex flex-col bg-white rounded-lg shadow-md p-6 items-center">
+        <div className="text-center mt-40">
+          <h2 className="text-3xl font-bold leading-tight text-sky-900 font-sans hover:text-sky-700">
+            Elf'in Şaka App'i
+          </h2>
+        </div>
+        <div className="flex flex-col items-center border border-sky-100 border-dashed shadow-2xl p-4 mt-7">
+          <div className="text-center space-y-4 mt-10">
+            <p className="text-xl text-sky-700 font-sans hover:text-sky-400">
+              {" "}
+              &quot;{currentJoke.text}&quot;
+            </p>
+          </div>
+          <div className="flex flex-col items-center justify-center ">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+              className="border rounded-md bg-white m-4 w-1/2 "
+              src={currentJoke.image}
+              width={1200}
+              height={1200}
+              alt="sakalar"
+            ></Image>
+          </div>
+          <div className="flex justify-center mt-4 space-x-4 mb-10">
+            <button
+              onClick={getNextJoke}
+              className="inline-block px-4 py-2 rounded-lg border border-gray-300 text-sky-800 font-semibold hover:text-sky-600 hover:bg-sky-100"
+            >
+              Yenisi
+              <span className="ml-2 text-sky-400" aria-hidden="true">
+                →
+              </span>
+            </button>
+
+            <Link
+              href={currentJoke.xLink}
+              className="inline-block px-4 py-2 rounded-lg border border-gray-300 text-sky-800 font-semibold hover:hover:text-sky-600 hover:bg-sky-100"
+            >
+              Tweete Git
+              <span className="ml-2 text-sky-400" aria-hidden="true">
+                →
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        <div className="text-center mt-10">
+          <span className="text-md font-italic leading-tight text-gray-400 ">
+            App'in geliştirilmesinde çok büyük katkıları olan <br></br>
+            <Link
+              className="font-bold text-sky-800 opacity-50 hover:opacity-90"
+              href="https://x.com/GweepCreative?s=20"
+            >
+              @gweepCreative
+            </Link>
+            'e özel teşekkürler ◕‿◕
+          </span>
+          <br></br>
+
+          <div className="inset-1 z-2 flex flex-col bg-gray rounded-lg shadow-sm p-3  mt-10">
+         
+            <span className="text-sm font-italic leading-tight text-gray-300 mt-10">
+              <Link
+                className="font-mono text-pink-400 opacity-50  hover:text-pink-600 opacity-25"
+                href="https://x.com/mommy4_0"
+              >
+                a @Mommy4_0 Project || 2024 || 4 my sweety sista!
+              </Link>
+            </span>
+   
+          </div>
+          
+          
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
