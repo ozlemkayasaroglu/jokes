925caffcd13e56026158fed724a1da9d2bc1821b
ozlemkayasaroglu <ozlemkayasaroglu@gmail.com>
2024-02-03 09:42:59 +0300

    mongodb

--- app/api/route.js
+++ app/api/route.js
@@ -1,15 +1,43 @@
 
+// import { MongoClient } from "mongodb";
+
+// export async function GET() {
+//   const mongodb = "mongodb://localhost:27017/jokes-db";
+//   const client = new MongoClient(mongodb);
+//   await client.connect();
+
+//   const db = client.db(); 
+//   const collection = db.collection("jokes"); 
+//   const data = await collection.find({}).toArray();
+
+//   return Response.json(data);
+
+// }
+
+
 import { MongoClient } from "mongodb";
 
-export async function GET() {
-  const mongodb = "mongodb://localhost:27017/jokes-db";
-  const client = new MongoClient(mongodb);
-  await client.connect();
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const mongodb = "mongodb://localhost:27017/jokes-db";
 
-  const db = client.db(); 
-  const collection = db.collection("jokes"); 
-  const data = await collection.find({}).toArray();
+    try {
+      const client = new MongoClient(mongodb, { useNewUrlParser: true, useUnifiedTopology: true });
+      await client.connect();
 
-  return Response.json(data);
+      const db = client.db();
+      const collection = db.collection("jokes");
+      const data = await collection.find({}).toArray();
 
+      res.status(200).json(data);
+    } catch (error) {
+      console.error("MongoDB connection error:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    } finally {
+     
+      await client.close();
+    }
+  } else {
+    res.status(405).json({ error: "Method Not Allowed" });
+  }
 }
