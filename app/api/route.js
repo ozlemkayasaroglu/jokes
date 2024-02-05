350b7f437bc87ba594ec49b0d94a6894a39a34bc
ozlemkayasaroglu <ozlemkayasaroglu@gmail.com>
2024-02-05 13:30:57 +0300

    local mongodb kaldırıldı

--- app/api/route.js
+++ app/api/route.js
@@ -1,43 +1,8 @@
+import path from "path";
+import { promises as fs } from "fs";
 
-// import { MongoClient } from "mongodb";
-
-// export async function GET() {
-//   const mongodb = "mongodb://localhost:27017/jokes-db";
-//   const client = new MongoClient(mongodb);
-//   await client.connect();
-
-//   const db = client.db(); 
-//   const collection = db.collection("jokes"); 
-//   const data = await collection.find({}).toArray();
-
-//   return Response.json(data);
-
-// }
-
-
-import { MongoClient } from "mongodb";
-
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    const mongodb = "mongodb://localhost:27017/jokes-db";
-
-    try {
-      const client = new MongoClient(mongodb, { useNewUrlParser: true, useUnifiedTopology: true });
-      await client.connect();
-
-      const db = client.db();
-      const collection = db.collection("jokes");
-      const data = await collection.find({}).toArray();
-
-      res.status(200).json(data);
-    } catch (error) {
-      console.error("MongoDB connection error:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    } finally {
-     
-      await client.close();
-    }
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
-  }
+export async function GET() {
+  const jsonDir = path.join(process.cwd(), "app");
+  const fileContents = await fs.readFile(jsonDir + "/component" + "/jokes.json", "utf8");
+  return Response.json({ data: JSON.parse(fileContents) });
 }
--- app/page.js
+++ app/page.js
@@ -4,44 +4,47 @@ import Image from "next/image";
 import Link from "next/link";
 import { useTheme } from "next-themes";
 
+
 export default function Home() {
-  const [data, setData] = useState([]);
+  const [jokes, setJokes] = useState([]);
   const [currentJokeIndex, setCurrentJokeIndex] = useState(0);
+
   const { theme, setTheme } = useTheme();
 
 
   const getNextJoke = () => {
-    const randomIndex = Math.floor(Math.random() * data.length);
+    const randomIndex = Math.floor(Math.random() * jokes.length);
     setCurrentJokeIndex(randomIndex);
-    console.log(randomIndex);
+  
   };
 
+
   useEffect(() => {
-    async function fetchData() {
+    async function fetchJokes() {
       try {
-        const response = await fetch("/api/");
+        const response = await fetch("/api");
         if (!response.ok) {
           throw new Error("veri alınamadı");
         }
-
-        const jsonData = await response.json();
-        setData(jsonData);
+        const data = await response.json();
+        setJokes(data.data);
       } catch (error) {
         console.error("fetch hatası", error);
       }
     }
-    fetchData();
+    fetchJokes();
   }, []);
 
-  if (data.length === 0) {
+
+  if (jokes.length === 0) {
     return <div>Şakalar yükleniyor...</div>;
   }
-  const currentJoke = data[currentJokeIndex];
+   const currentJoke = jokes[currentJokeIndex];
 
 
   return (
     <>
-      <div className="inset-0 z-10 flex flex-col bg-white dark:bg-sky-900 rounded-lg shadow-md dark:shadow-md p-6 items-center">
+     <div className="inset-0 z-10 flex flex-col bg-white dark:bg-sky-900 rounded-lg shadow-md dark:shadow-md p-6 items-center">
         <div className="flex items-center justify-between mt-4">
             <h2 className="text-3xl font-bold leading-tight text-sky-900 font-sans hover:text-sky-700 dark:text-white ">
               Elf&lsquo;in App&rsquo;i
@@ -64,10 +67,10 @@ export default function Home() {
                 </span>
               </span>
             </label>
-        </div>
+        </div> 
 
         {/* Joke Display */}
-        <div className="flex flex-col items-center border border-sky-100 border-dashed shadow-2xl p-4 mt-7 dark:bg-sky-900 dark:border-yellow-600">
+       <div className="flex flex-col items-center border border-sky-100 border-dashed shadow-2xl p-4 mt-7 dark:bg-sky-900 dark:border-yellow-600">
           <div className="text-center space-y-4 mt-10">
             <p className="text-3xl text-sky-700 font-sans hover:text-sky-400 dark:text-yellow-400 ">
               &quot;{currentJoke.text}&quot;
@@ -106,7 +109,7 @@ export default function Home() {
         </div>
 
         {/* Footer */}
-        <div className="text-center mt-3">
+     <div className="text-center mt-3">
           <span className="text-xs font-italic leading-tight text-gray-400 dark:text-zinc-400 dark:hover:text-zinc-300">
             App&lsquo;in geliştirilmesinde çok büyük katkıları olan <br></br>
             <Link
@@ -130,7 +133,7 @@ export default function Home() {
             </span>
           </div>
         </div>
-      </div>
+      </div> 
     </>
   );
 }
